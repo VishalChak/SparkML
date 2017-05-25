36835e46b6e9ad56ad053794bc870d19b1dd77ac
VishalChak <vishalbabu.in@gmail.com>
2017-05-25 18:05:37 +0530

    List to dataset and List to dataFrame

--- src/main/java/sparkML/Test.java
+++ src/main/java/sparkML/Test.java
@@ -4,20 +4,33 @@ package sparkML;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.spark.ml.linalg.VectorUDT;
+import org.apache.spark.ml.linalg.Vectors;
 import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 
 public class Test {
 	public static void main(String args[]){
-		SparkSession session = SparkSession.builder().master("local").appName("Test").getOrCreate();
-		List<String> data = Arrays.asList("Vishal","Babu", "Chak");
+		SparkSession session = SparkSession.builder().master("local[*]").appName("Test").getOrCreate();
 		
-//		 List<Row>data = Arrays.asList(
-//				 RowFactory.create(Vectors.dense(1,2,3,4,5)),
-//				 RowFactory.create(Vectors.dense(5,4,3,2,1))
-//				 );
-		 Dataset<String> ds = session.createDataset(data,Encoders.STRING());
+		 List<Row>data = Arrays.asList(
+				 RowFactory.create(Vectors.dense(1,2,3,4,5)),
+				 RowFactory.create(Vectors.dense(5,4,3,2,1))
+				 );
+		 
+		 StructType schema = new StructType(new StructField[]{
+			      new StructField("features", new VectorUDT(), false, Metadata.empty()),
+			    });
+		 
+		 Dataset<Row> ds = session.createDataFrame(data, schema);
+		 long count = ds.count();
+		 System.out.println(count);
 		 ds.show();
+		 session.stop();
 	}
 }
