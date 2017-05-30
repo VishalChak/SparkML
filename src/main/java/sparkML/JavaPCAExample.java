8d824d9e3afc17e1e70f02dda0e5f69ef6fe3fd0
VishalChak <vishalbabu.in@gmail.com>
2017-05-30 11:08:35 +0530

    PCA from file

--- src/main/java/sparkML/JavaPCAExample.java
+++ src/main/java/sparkML/JavaPCAExample.java
@@ -1,12 +1,9 @@
 package sparkML;
 
 
-import org.apache.spark.sql.SparkSession;
-
-// $example on$
-import java.util.Arrays;
-import java.util.List;
-
+import org.apache.commons.beanutils.ConvertUtils;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.ml.feature.PCA;
 import org.apache.spark.ml.feature.PCAModel;
 import org.apache.spark.ml.linalg.VectorUDT;
@@ -14,6 +11,7 @@ import org.apache.spark.ml.linalg.Vectors;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.types.Metadata;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
@@ -21,32 +19,34 @@ import org.apache.spark.sql.types.StructType;
 
 public class JavaPCAExample {
   public static void main(String[] args) {
-	  System.setProperty("hadoop.home.dir", "D:/Vishal/hadoopWinUtill/bin");
+	
     SparkSession spark = SparkSession.builder().master("local").appName("JavaPCAExample").getOrCreate();
-
-//    List<Row> data = Arrays.asList(
-//      RowFactory.create(Vectors.sparse(5, new int[]{1, 3}, new double[]{1.0, 7.0})),
-//      RowFactory.create(Vectors.dense(2.0, 0.0, 3.0, 4.0, 5.0)),
-//      RowFactory.create(Vectors.dense(4.0, 0.0, 0.0, 6.0, 7.0))
-//    );
-//    
+    System.setProperty("hadoop.home.dir", "D:/Vishal/hadoopWinUtill/");
+    
     StructType schema = new StructType(new StructField[]{
       new StructField("features", new VectorUDT(), false, Metadata.empty()),
     });
 	  String titanic_ph = "D:/Vishal/Kaggle_Competition/Titanic/test.csv";
-//	  Dataset<Row> df = spark.read().json("examples/src/main/resources/people.json");
-	  Dataset<Row> df = spark.read().json(titanic_ph);
-	  df.show();
-//	  Dataset<Row> df = spark.createDataFrame(data, schema);
-/*
+	  JavaRDD<String> rdd = spark.read().textFile(titanic_ph).toJavaRDD();
+	  
+	  JavaRDD<Row> rdd1 = rdd.map(new Function<String, Row>() {
+
+		public Row call(String arg0) throws Exception {
+			String [] arr = arg0.split(",");
+			double[] doubleValues = (double[])ConvertUtils.convert(arr, Double.TYPE);
+			return RowFactory.create(Vectors.dense(doubleValues));
+		}
+	});
+	 Dataset<Row> dataset= spark.createDataFrame(rdd1, schema);
+
     PCAModel pca = new PCA()
       .setInputCol("features")
       .setOutputCol("pcaFeatures")
-      .setK(3)
-      .fit(df);
+      .setK(1)
+      .fit(dataset);
 
-    Dataset<Row> result = pca.transform(df).select("pcaFeatures");
-    result.show();*/
+    Dataset<Row> result = pca.transform(dataset).select("pcaFeatures");
+    result.show();
     spark.stop();
   }
 }
