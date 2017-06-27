a7ec8e5e4149efaca30b40aa781a8201664e81a0
VishalChak <vishalbabu.in@gmail.com>
2017-06-27 12:55:20 +0530

    dd

--- src/main/java/Utility/Schema.java
+++
@@ -1,8 +0,0 @@
-package Utility;
-
-public class Schema {
-//	StructType schema = DataTypes.createStructType(new StructField[]{
-////new StructField("label", DataTypes.DoubleType, false, new MetadataBuilder().build()),
-//new StructField("features", new VectorUDT(), false, new MetadataBuilder().build())
-//});	
-}
