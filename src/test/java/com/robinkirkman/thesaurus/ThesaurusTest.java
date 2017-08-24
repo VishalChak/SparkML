19f7f37f71179e1c6eff699de924c40c5c8abfc5
VishalChak <vishalbabu.in@gmail.com>
2017-08-24 19:28:44 +0530

    bb

--- src/test/java/com/robinkirkman/thesaurus/ThesaurusTest.java
+++ src/test/java/com/robinkirkman/thesaurus/ThesaurusTest.java
@@ -1,13 +1,16 @@
 package com.robinkirkman.thesaurus;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class ThesaurusTest {
 	@Test
 	public void testHi() {
-		String word = "Temple".toLowerCase();
+		String word = "temple".toLowerCase();
 		if (null != Thesaurus.getInstance().get(word)){
 			System.out.println(Thesaurus.getInstance().get(word).size());
+//			Arrays.sort();
 		}
 		System.out.println(Thesaurus.getInstance().get(word));
 	}
